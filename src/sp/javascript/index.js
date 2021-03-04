c6e13959760b1c1476bc7364132acf0033c830ee
hikari <tanimoto0709@gmail.com>
2021-03-05 08:19:36 +0900

    update:main FLOCSSでのコーディング環境の作成

--- src/sp/javascript/index.js
+++ src/sp/javascript/index.js
@@ -1,3 +1,4 @@
 import { test } from './component/test'; //拡張子は省略可能
+import '../stylesheet/style.scss';
 
 test();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const pc = {
   mode: 'production',
   entry: './src/pc/index.js',
   output: {
-    path: __dirname + '/docroot/dist',
+    path: path.resolve(__dirname, 'docroot/dist'),
     filename: 'index.js'
   },
   watch: true,
@@ -13,11 +16,59 @@ const pc = {
 
 const sp = {
   mode: 'production',
-  entry: './src/sp/index.js',
+  entry: './src/sp/javascript/index.js',
   output: {
-    path: __dirname + '/docroot_sp/dist',
-    filename: 'index.js'
+    path: path.resolve(__dirname, 'docroot_sp/dist'),
+    filename: 'javascript/index.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+              },
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    })
+  ],
   watch: true,
   watchOptions: {
     ignored: /node_modules/
